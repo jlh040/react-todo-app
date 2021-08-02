91f8f31cf4a90d881689eb506ac548ece0f8517c
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-02 16:27:13 -0700

    TodoList component now returns some html

---
+++ src/TodoList.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([{task: 'clean the gutters', id: uuid()}]);
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      {todos.map(todo => <Todo id={todo.id} task={todo.task}/>)}
+    </div>
+  )
+};
+
+export default TodoList;
