ccb0effe9e2d6473cf74d72a86cc44b7ae298f11
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-02 16:43:08 -0700

    added change handler for the NewTodoForm

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -5,7 +5,28 @@ const NewTodoForm = () => {
     task: 'Where are the goos'
   };
   const [formData, setFormData] = useState(initialState);
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormData({
+      [name]: value
+    })
+  }
 
+  return (
+    <div>
+      <form>
+        <label htmlFor="task">Enter task: </label>
+        <input 
+          type="text" 
+          id="task" 
+          placeholder="Enter task here" 
+          name="task" 
+          value={formData.task}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  )
 };
 
 export default NewTodoForm;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuid } from 'uuid';
+import NewTodoForm from './NewTodoForm';
 import Todo from './Todo';
 
 const TodoList = () => {
@@ -8,6 +9,7 @@ const TodoList = () => {
   return (
     <div>
       <h2>Todo List</h2>
+      <NewTodoForm />
       {todos.map(todo => <Todo id={todo.id} task={todo.task}/>)}
     </div>
   )
