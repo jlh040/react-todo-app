bbe2eefcb67db2de556b5be8f0a1b898e85c9d44
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-02 17:53:04 -0700

    added a test to make sure todos show up on the page after being submitted

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -15,4 +15,15 @@ it('does not show todos upon page load', () => {
     const todoParagraph = queryByText('Task:');
 
     expect(todoParagraph).not.toBeInTheDocument();
+});
+
+it('renders todos upon submit', () => {
+    const { getByLabelText, getByText } = render(<TodoList />);
+    const taskInput = getByLabelText('Enter task:');
+    const btn = getByText('Add Task');
+
+    fireEvent.change(taskInput, {target: {value: 'Walk the dog'}});
+    fireEvent.click(btn);
+
+    expect(getByText('Task: Walk the dog')).toBeInTheDocument()
 })
