23522854bbf502522c68fdfb0aadba6cc5f97943
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-02 18:02:39 -0700

    wrote a test to check that todos can successfully be removed from the page

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -26,4 +26,19 @@ it('renders todos upon submit', () => {
     fireEvent.click(btn);
 
     expect(getByText('Task: Walk the dog')).toBeInTheDocument()
+});
+
+it('successfully removes todos from the page', () => {
+    const { getByLabelText, getByText, queryByText } = render(<TodoList />);
+    const taskInput = getByLabelText('Enter task:');
+    const submitBtn = getByText('Add Task');
+
+    fireEvent.change(taskInput, {target: {value: 'Take out the trash'}})
+    fireEvent.click(submitBtn);
+
+    expect(getByText('Task: Take out the trash')).toBeInTheDocument();
+
+    fireEvent.click(getByText('X'));
+
+    expect(queryByText('Task: Take out the trash')).not.toBeInTheDocument();
 })
